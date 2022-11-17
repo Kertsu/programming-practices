63b9ede1310f8284433069541c38ea57e24163eb
Kertsu <kurtddbigtas@gmail.com>
2022-11-17 23:02:13 +0800

    added duplicate letter finder

---
+++ duplicate_letter_finder.py
@@ -0,0 +1,17 @@
+word = input('Enter any word: ')
+
+def find_dupe():
+    for i in range(len(word)):
+        current_letter = word[i]
+        for j in range(i, len(word)-1):
+            next_letter = word[j+1]
+            if next_letter == current_letter:
+                global duplicate_letter
+                duplicate_letter = current_letter
+                return True
+    return False
+
+if find_dupe():
+    print("A letter has a duplicate.")
+else:
+    print('No duplicate letters.')
