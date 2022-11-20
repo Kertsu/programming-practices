c877d44531fc510f028399ce3954611adbbc1d20
Kertsu <kurtddbigtas@gmail.com>
2022-11-20 08:45:10 +0800

    edited recursive find min

--- recursive_find_min.py
+++ recursive_find_min.py
@@ -1,16 +1,16 @@
 list = [3,5,7,1,10,25,45,2,4,24,76,1521,52]
 
-def find_max(list, idx, minimum_val):
+def find_min(list, idx, minimum_val):
     if idx < len(list):    
         if minimum_val == list[idx]:
-            return find_max(list, idx+1, minimum_val)
+            return find_min(list, idx+1, minimum_val)
         if minimum_val < list[idx]:
-            return find_max(list, idx+1, minimum_val)
+            return find_min(list, idx+1, minimum_val)
         if minimum_val > list[idx]:
-            return find_max(list, idx+1, list[idx])
+            return find_min(list, idx+1, list[idx])
 
     return minimum_val 
 
-result = find_max(list, 0, list[0])
+result = find_min(list, 0, list[0])
 if result > 0:
     print(result)
